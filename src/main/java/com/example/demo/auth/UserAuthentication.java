7903a838c9c04eac5d102bb28cff7d54ae6dd45d
heta08 <68490383+heta08@users.noreply.github.com>
2022-04-17 18:37:44 -0400

    Update UserAuthentication.java

--- src/main/java/com/example/demo/auth/UserAuthentication.java
+++ src/main/java/com/example/demo/auth/UserAuthentication.java
@@ -1,16 +1,15 @@
-<<<<<<< Updated upstream
-package com.example.demo.auth;
+package com.group5.librarymanagement.auth;
 
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.example.demo.entity.User;
+import com.group5.librarymanagement.entity.User;
 
 public class UserAuthentication implements UserDetails {
 	
-	private User user;
+private User user;
 	
 	
 
@@ -66,72 +65,3 @@ public class UserAuthentication implements UserDetails {
 	}
 
 }
-=======
-package com.example.demo.auth;
-
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import com.example.demo.entity.User;
-
-public class UserAuthentication implements UserDetails {
-	
-	private User user;
-	
-	
-
-	public UserAuthentication(User user) {
-		super();
-		this.user = user;
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return user.getPassword();
-	}
-
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return user.getEmail();
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-	
-	public String getFullName() {
-		return user.getFirstName() + " " +user.getLastName();
-	}
-
-}
->>>>>>> Stashed changes
