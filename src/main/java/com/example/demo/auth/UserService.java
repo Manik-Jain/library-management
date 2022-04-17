321981e557588214cda020e2914c98c7750c7171
heta08 <68490383+heta08@users.noreply.github.com>
2022-04-17 18:38:23 -0400

    Update UserService.java

--- src/main/java/com/example/demo/auth/UserService.java
+++ src/main/java/com/example/demo/auth/UserService.java
@@ -1,13 +1,13 @@
-<<<<<<< Updated upstream
-package com.example.demo.auth;
+package com.group5.librarymanagement.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import com.example.demo.entity.User;
-import com.example.demo.repo.UserRepository;
+
+import com.group5.librarymanagement.entity.User;
+import com.group5.librarymanagement.repositories.UserRepository;
 
 public class UserService implements UserDetailsService {
 	
@@ -23,33 +23,4 @@ public class UserService implements UserDetailsService {
 		}
 		return new UserAuthentication(user);
 	}
-
 }
-=======
-package com.example.demo.auth;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import com.example.demo.entity.User;
-import com.example.demo.repo.UserRepository;
-
-public class UserService implements UserDetailsService {
-	
-	@Autowired
-	private UserRepository userRepo;
-
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		User user = userRepo.findByEmail(email);
-		if(user == null) {
-			throw new UsernameNotFoundException("User not found");
-		}
-		return new UserAuthentication(user);
-	}
-
-}
->>>>>>> Stashed changes
